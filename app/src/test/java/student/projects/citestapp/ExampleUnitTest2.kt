9137d5eba6cf0daee534f0d6456462115df15a4c
ST10211930 <129373497+ST10211930@users.noreply.github.com>
2025-11-18 15:28:28 +0200

    Initial Commit

---
+++ app/src/test/java/student/projects/citestapp/ExampleUnitTest2.kt
@@ -0,0 +1,13 @@
+package student.projects.citestapp
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ExampleUnitTest2 {
+
+    @Test
+    fun addition_isCorrect() {
+        val result = 2 + 3
+        assertEquals(5, result)
+    }
+}
